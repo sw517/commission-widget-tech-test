cfc3c43d09c2aa0369638b2ffd82cdb485108bd3
Sam Wood <16246185+sw517@users.noreply.github.com>
2024-01-31 16:54:25 +0000

    Init

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '.+\\.(svg|png|jpg)$': 'identity-obj-proxy',
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+  },
+};
